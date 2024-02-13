c921a4044173103f9948299e88f893800573c2ba
root <ELIAKIM1520>
2024-02-13 00:17:06 -0800

    python rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" To write an empty class rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    "an empty class that defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+echo "0x08. Python - More Classes and Objects "
