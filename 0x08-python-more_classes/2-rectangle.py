053b3f27f09d50a91b1d592c144481b60eb92001
root <ELIAKIM1520>
2024-02-13 00:50:41 -0800

    more

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+""" Writing a class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """it defines a rectangle based on the 1-rectangle.py
+"""
+    def __init__(self, width=0, height=0):
+        """Insatntiate of our class object
+        Args:
+            width (int): the value of the width of rectangle
+            size (int): the value of the height of rectangle
+"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter method for width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method fot width of rectangle
+        Args:
+            value (int): the value to input as width
+"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height of the rectangle
+        Args:
+        value (int): the value to set as height of rectangle
+"""
+        self.__height = value
+
+    def area(self):
+        """calculates the area of a rectangle
+        Returns:
+            int: the area of rectangle
+"""
+        a = self.height * self.width
+        return a
+
+    def perimeter(self):
+        """Returns the rectangular perimeter"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        else:
+            prm = (self.height * 2) + (self.width * 2)
+    @property
+    def height(self):
+        """Getter for the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height of the rectangle
+        Args:
+        value (int): the value to set as height of rectangle
+"""
+        self.__height = value
+
+    def area(self):
+        """calculates the area of a rectangle
+        Returns:
+            int: the area of rectangle
+"""
+        a = self.height * self.width
+        return a
+  
+    def perimeter(self):
+        """Returns the rectangular perimeter"""
+        if self.height == 0 or self.width == 0:
+          return 0
+        else:
+            prm = (self.height * 2) + (self.width * 2)
+            return (prm)
