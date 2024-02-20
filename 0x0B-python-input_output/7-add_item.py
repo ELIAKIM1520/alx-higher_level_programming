0529bd46f4991209aaa519bd6f1200d3002ad66a
root <ELIAKIM1520>
2024-02-20 01:07:36 -0800

    merge

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+""" it adds all the arguments to a python list, and then save
+them to a file"""
 import sys
 
 if __name__ == "__main__":
+    """ adds all the arguments to a python list
+"""
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
