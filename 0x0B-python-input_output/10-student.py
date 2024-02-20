7d4f596dd9b656deb7e4c51a79e0e8fe40fafba6
root <ELIAKIM1520>
2024-02-20 01:10:04 -0800

    10 students

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,31 @@
-
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Defines a class Student by (based on 9-student.py)
+"""
 
 
 class Student:
-    """Represent a student."""
+    """class student based on the 9-student.py
+"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """instantiation of our object
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        first_name (str): the first name of the student
+        last_name (str): the last_name of the student
+        age (int): the age of the student
+"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+        
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+        """retrieves a dictionary represantation of a student instance
+        if attrs is a list of strings, it represents only those attributes included in the list
+
         Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
+            attrs (list):attributes to represent
+"""
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+                all(type(item) == str for item in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.
+        return self.__dict__
