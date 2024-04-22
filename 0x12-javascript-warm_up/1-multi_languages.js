2d9f39e102f75296deb0a7174ef2482ba11ec7a6
ELIAKIM1520 <ivashaeliakim@gmail.com>
2024-04-22 15:11:55 +0000

    module

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@ const myVar1 = "C is fun"
 const myVar2 = "Python is cool"
 const myVar3 = "JavaScript is amazing"
 
-console.log(firstline);
-console.log(secondline);
-console.log(thirdline);
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
