5368b77fdfd15dbed393dd44de5039e407c07c4b
ELIAKIM1520 <ivashaeliakim@gmail.com>
2024-04-23 13:15:45 +0000

    create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-
-if (process.argv[2] && process.argv[3]) {
-    console.log(process.argv[2] + " is " + process.argv[3]);
-}
-else if (process.argv.length === 3) {
-    console.log(process.argv[2] + " is " + "undefined");
-}
-else {
-    console.log("undefined is undefined");
-}
+const array = process.argv.slice(2);
+console.log(`${array[0]} is ${array[1]}`);
