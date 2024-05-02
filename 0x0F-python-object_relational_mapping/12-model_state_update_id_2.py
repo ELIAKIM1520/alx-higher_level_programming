0e20ac0969a5943ecf0de0d1ec5604e29f15453f
root <ELIAKIM1520>
2024-05-02 05:05:28 -0700

    update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Module that updates the name of a state in a\
-        MySQL database using SQLAlchemy."""
+""" Script that changes the name of a State object from the db"""
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
+Base = declarative_base()
+
+class State(Base):
+    """Define the State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    # Create a session factory
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    # Create a session object
     session = Session()
-
-    # Retrieve the state with ID 2 from the database
     state = session.query(State).filter_by(id=2).first()
-    # Update the name of the state to "New Mexico"
     state.name = "New Mexico"
-    # Commit the session to persist the changes
     session.commit()
 
