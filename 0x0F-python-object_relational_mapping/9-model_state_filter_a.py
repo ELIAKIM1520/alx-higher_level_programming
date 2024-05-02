e97e0c43145f26ab2741063473fabfa046b28643
root <ELIAKIM1520>
2024-05-02 04:15:09 -0700

    first state

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints the states with\
-        letter a from a MySQL database using SQLAlchemy."""
+"""Lists all State objects that contain a letter a from the database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # Create a session factory
     Session = sessionmaker(bind=engine)
-
-    # Create a session object
     session = Session()
 
-    # Retrieve the states with letter 'a' from the
-    # database and print its ID and name
     for state in session.query(State).order_by(State.id):
         if "a" in state.name:
             print("{}: {}".format(state.id, state.name))
-
