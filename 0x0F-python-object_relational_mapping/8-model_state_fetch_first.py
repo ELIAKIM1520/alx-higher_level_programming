cf59222451c26bd861c31935243a56d60a71bb6e
root <ELIAKIM1520>
2024-05-02 04:46:50 -0700

    first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """Script that prints the first State object from the database"""
 import sys
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Define the State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
 
 if __name__ == '__main__':
     username, password, database = sys.argv[1], sys.argv[2],  sys.argv[3]
@@ -12,8 +20,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
     f_state = session.query(State).order_by(State.id).first()
-    if (f_state):
+    if f_state:
         print(f'{f_state.id}: {f_state.name}')
     else:
         print('Nothing')
     session.close()
+
